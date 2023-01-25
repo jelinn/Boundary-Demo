b0db35a2c241028dafc99845e52cbc8cb0cc4513
Justin Linn <jelinn@gmail.com>
2023-01-25 14:26:46 -0600

    add aws dir - dynamic

---
+++ aws/main.tf
@@ -0,0 +1,14 @@
+# Configure the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.73"
+      }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+    region = var.aws_region
+}
---
+++ boundary/boundary_aws.tf
@@ -0,0 +1,16 @@
+
+
+resource "boundary_host_catalog_plugin" "aws_example" {
+  name            = "personal-aws-us-east-1"
+  description     = "Personal cloud resources - us-east-1"
+  scope_id        = boundary_scope.cloud_sarvices.id
+  plugin_name     = "aws"
+  attributes_json = jsonencode({ "region" = "us-east-1" })
+
+  # recommended to pass in aws secrets using a file() or using environment variables
+  # the secrets below must be generated in aws by creating a aws iam user with programmatic access
+  secrets_json = jsonencode({
+    "access_key_id"     = env://BOUNDARY_ACCESS_KEY_ID,
+    "secret_access_key" = env://BOUNDARY_SECRET_ACCESS_KEY
+  })
+}
