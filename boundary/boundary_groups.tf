7758e7d5d53497e86b8c0ebca28f578322f6bc3d
Justin Linn <jelinn@gmail.com>
2023-01-25 11:18:01 -0600

    add groups example

--- boundary/boundary_groups.tf
+++ boundary/boundary_groups.tf
@@ -5,6 +5,20 @@ resource "boundary_group" "admin" {
   scope_id    = boundary_scope.personal.id
 }
 
+resource "boundary_group" "admin_cloud" {
+  name        = "cloud_admin"
+  description = "Admin users - Cloud"
+  member_ids  = [boundary_user.jelinn.id]
+  scope_id    = boundary_scope.cloud_services.id
+}
+
+resource "boundary_group" "admin_home" {
+  name        = "home_admin"
+  description = "Admin users - Home"
+  member_ids  = [boundary_user.jelinn.id]
+  scope_id    = boundary_scope.home_servers.id
+}
+
 resource "boundary_group" "cloud" {
   name        = "cloud"
   description = "Cloud Services"
