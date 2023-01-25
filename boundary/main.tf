6cad2f3397e35bff800cdb657c06edddd6e2f527
Justin Linn <jelinn@gmail.com>
2023-01-24 19:09:23 -0600

    test tfc data source

--- boundary/main.tf
+++ boundary/main.tf
@@ -6,8 +6,13 @@ terraform {
 }
 }
 
+data "tfe_outputs" "boundary_deploy" {
+  organization = var.tfc_org
+  workspace = var.boundary_deploy_workspace
+}
+
 provider "boundary" {
-  addr                            = var.boundary_addr
+  addr                            = data.tfe_outputs.boundary_deploy.Boundary_URL
   auth_method_id                  = var.boundary_auth_method_id
   password_auth_method_login_name = var.boundary_user_name 
   password_auth_method_password   = var.boundary_password 
--- boundary/variables.tf
+++ boundary/variables.tf
@@ -10,3 +10,10 @@ variable boundary_user_name{
 variable boundary_password{
     
 }
+variable tfc_org{
+    default="jlinn-test"
+}
+
+variable boundary_deploy_workspace{
+    default = "Boundary-Demo-Deploy"
+}
