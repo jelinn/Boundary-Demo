b4edb135faadb88efee31c7314449852b77cba46
Justin Linn <jelinn@gmail.com>
2023-01-25 12:05:59 -0600

    test roles

--- boundary/boundary_roles.tf
+++ boundary/boundary_roles.tf
@@ -11,7 +11,7 @@ resource "boundary_role" "manage_cloud" {
   name        = "manage_cloud"
   description = "Manage cloud resources"
   scope_id    = boundary_scope.cloud_services.id
-  principal_ids   = [boundary_group.admin_cloud.id]
+  principal_ids   = [boundary_user.bob_test_account.id]
   grant_strings   = ["id=*;type=*;actions=read,list"]
 }
 
@@ -19,6 +19,6 @@ resource "boundary_role" "manage_home" {
   name        = "manage_home"
   description = "Manage home resources"
   scope_id    = boundary_scope.home_servers.id
-  principal_ids   = [boundary_group.admin_home.id]
+  principal_ids   = [boundary_user.alice_test_account.id]
   grant_strings   = ["id=*;type=*;actions=read,list"]
 }
