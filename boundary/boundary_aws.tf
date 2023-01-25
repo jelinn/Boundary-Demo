cd3338b12be9e8e22d972419984248f8f4e58b92
Justin Linn <jelinn@gmail.com>
2023-01-24 21:06:44 -0600

    add dynamic AWS

--- boundary/boundary_aws.tf
+++ boundary/boundary_aws.tf
@@ -8,7 +8,7 @@ resource "boundary_host_catalog_plugin" "aws_example" {
   # recommended to pass in aws secrets using a file() or using environment variables
   # the secrets below must be generated in aws by creating a aws iam user with programmatic access
   secrets_json = jsonencode({
-    "access_key_id"     = ${var.boundary_aws_access_key},
-    "secret_access_key" = ${var.boundary_aws_secret_access_key_value}
+    "access_key_id"     = var.boundary_aws_access_key,
+    "secret_access_key" = var.boundary_aws_secret_access_key_value
   })
 }
