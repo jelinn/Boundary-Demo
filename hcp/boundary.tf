9c46d17a510e7fc271d0396b7892a071c5426dd5
Justin Linn <jelinn@gmail.com>
2023-01-24 18:28:17 -0600

    add cluster

---
+++ hcp/boundary.tf
@@ -0,0 +1,5 @@
+resource "hcp_boundary_cluster" "example" {
+  cluster_id = var.boundary_cluster_name
+  username   = var.boundary_user_name
+  password   = var.boundary_password
+}
---
+++ hcp/variables.tf
@@ -0,0 +1,3 @@
+variable boundary_cluster_name{}
+variable boundary_user_name{}
+variable boundary_password{}
