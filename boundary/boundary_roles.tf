c8a5124fcf9b63947ca8ce2f7a865465465974ba
Justin Linn <jelinn@gmail.com>
2023-01-25 11:33:06 -0600

    add roles

---
+++ boundary/boundary_roles.tf
@@ -0,0 +1,24 @@
+
+resource "boundary_role" "manage_personal" {
+  name        = "manage_personal"
+  description = "Manage all personal resources"
+  scope_id    = boundary_scope.personal.id
+  principal_ids   = [boundary_group.admin.id]
+  grant_strings   = ["id=*;type=*;actions=read,list"]
+}
+
+resource "boundary_role" "manage_cloud" {
+  name        = "manage_cloud"
+  description = "Manage cloud resources"
+  scope_id    = boundary_scope.cloud_services.id
+  principal_ids   = [boundary_group.cloud_admin.id]
+  grant_strings   = ["id=*;type=*;actions=read,list"]
+}
+
+resource "boundary_role" "manage_home" {
+  name        = "manage_home"
+  description = "Manage home resources"
+  scope_id    = boundary_scope.home_servers.id
+  principal_ids   = [boundary_group.home_admin.id]
+  grant_strings   = ["id=*;type=*;actions=read,list"]
+}
