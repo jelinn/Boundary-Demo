6dfd4a112595d6b0d02b7c1933777b076631b853
Justin Linn <jelinn@gmail.com>
2023-08-15 09:05:45 -0500

    update variable name

--- hcp/boundary.tf
+++ hcp/boundary.tf
@@ -2,5 +2,5 @@ resource "hcp_boundary_cluster" "example" {
   cluster_id = var.boundary_cluster_name
   username   = var.boundary_user_name
   password   = var.boundary_password
-  tier 	     = var.tier
+  tier 	     = var.boundary_tier
 }
